8ddb58b260bd14fdb7a62721d5a959794fe1bcb5
DarkEye <lesko.matej.pu@gmail.com>
2018-08-05 18:02:11 +0200

    deleting functionality of quotes added

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <app-grid></app-grid>
+  </div>
+</template>
+
+<script>
+import Grid from './components/Grid'
+export default {
+  components: {
+    appGrid: Grid
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- for an explanation of index key see: https://vuejs.org/v2/guide/list.html#key -->
+        <app-quote v-for="(quote, index) in quotes" :key="index" @click.native="deleteQuote(index)"> {{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote'
+export default {
+  data() {
+    return {
+      quotes: ['default quote'],
+    }
+  },
+  methods: {
+      deleteQuote(index) {
+        console.log("deleting quote on index:", index);
+        this.quotes.splice(index, 1);
+      }
+  },
+  components: {
+      appQuote: Quote
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
